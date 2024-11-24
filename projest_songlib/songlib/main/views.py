3dc94dd02034575f2bcfdf25cca953a1ebc17230
Ivan Marchenko <143263454+SwogiK@users.noreply.github.com>
2024-11-24 12:58:56 +0300

    test version 2 of our new site
    template onlywith search bar

--- projest_songlib/songlib/main/views.py
+++ projest_songlib/songlib/main/views.py
@@ -8,3 +8,11 @@ def about(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def search(request):
+    query = request.GET.get('q')
+    # Здесь вы можете добавить логику поиска
+    # Например, поиск по базе данных или другому источнику данных
+    # В данном примере просто возвращаем результаты поиска в шаблон search.html
+    results = []  # Замените на реальные результаты поиска
+    return render(request, 'search.html', {'query': query, 'results': results})
